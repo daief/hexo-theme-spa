f194ba9ba53fdc8fff40394204ed9ea38896e4c8
daief <1437931235@qq.com>
2021-04-05 17:10:05 +0800

    wip: 返回顶部

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -11,7 +11,7 @@ if (!__SSR__) {
   const script = document.createElement('script');
   script.async = true;
   script.defer = true;
-  script.src = '//at.alicdn.com/t/font_2433925_4kmoqm3yvw2.js';
+  script.src = '//at.alicdn.com/t/font_2433925_4gal9ckxxra.js';
   document.body.append(script);
 }
 
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -24,9 +24,24 @@
   </div>
 
   <!-- fixed buttons -->
-  <div class="fixed bottom-2 right-2 text-2xl">
-    <button @click="showSideBar = true" v-if="isMobile">
-      <Icon name="home" />
+  <div
+    class="fixed bottom-3 right-3 z-10 text-3xl text-white lg:bottom-10 lg:right-10"
+  >
+    <transition name="fade">
+      <button
+        class="fixed-side-button"
+        v-show="scrollTop > 300"
+        @click="handleClickToTop"
+      >
+        <Icon name="top" />
+      </button>
+    </transition>
+    <button
+      class="fixed-side-button"
+      @click="showSideBar = true"
+      v-if="isMobile"
+    >
+      <Icon name="bars" />
     </button>
   </div>
 </template>
@@ -45,6 +60,8 @@ import Icon from './Icon.vue';
 import SideBarMobile from './SideBar/Mobile.vue';
 import SideBarPc from './SideBar/PC.vue';
 import SiteTitle from '@/components/site/SiteTitle.vue';
+import { useEventListener } from '@vant/use';
+import throttle from 'lodash/throttle';
 
 const { isMobile } = useBreakpointPrefix();
 const showSideBar = ref(!isMobile.value);
@@ -54,10 +71,43 @@ watch(isMobile, () => {
     showSideBar.value = true;
   }
 });
+
+const scrollTop = ref(0);
+
+useEventListener(
+  'scroll',
+  throttle(e => {
+    scrollTop.value = document.documentElement.scrollTop;
+  }),
+);
+
+const handleClickToTop = () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
 </script>
 
 <style lang="less" scoped>
 .main-page-content {
   min-height: calc(~'100vh - 90px');
+}
+.fixed-side-button {
+  @apply rounded-full;
+  @apply bg-black;
+  @apply bg-opacity-50;
+  @apply block;
+  @apply w-12;
+  @apply h-12;
+  @apply font-medium;
+  @apply shadow;
+  @apply flex;
+  @apply justify-center;
+  @apply items-center;
+
+  & + & {
+    @apply mt-4;
+  }
 }
 </style>
